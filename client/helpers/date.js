6da29f3f0f499aa434a228523e54e59b15793ec5
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-04 15:36:06 +0200

    Create helper to format date as dateTime

--- client/helpers/date.js
+++ client/helpers/date.js
@@ -1,6 +1,32 @@
-Template.registerHelper('prettifyDate', function(date) {
+errorIfNoDate = function (date) {
+    if (!(date instanceof Date)) {
+        throw "Object is not a Date.";
+    }
+};
+
+getDateFormat = function (date) {
+    errorIfNoDate(date);
     var day = date.getDate();
     var month = date.getMonth() + 1;
     var year = date.getFullYear();
     return year + '-' + ((month < 10) ? '0' : '') + month + '-' + ((day < 10) ? '0' : '') + day;
+};
+
+getDateTimeFormat = function (date) {
+    errorIfNoDate(date);
+    var formattedDate = getDateFormat(date);
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    formattedDate += ', ' + ((hours < 10) ? '0' : '') + hours + ':' + ((minutes < 10) ? '0' : '') + minutes;
+    return formattedDate;
+}
+
+// format date with format 'yyyy-MM-dd'
+Template.registerHelper('getDateFormat', function(date) {
+    return getDateFormat(date);
+});
+
+// format date with format 'yyyy-MM-dd, HH:ii'
+Template.registerHelper('getDateTimeFormat', function(date) {
+    return getDateTimeFormat(date);
 });
