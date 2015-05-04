b7eb996d66d0a0ce0eddf9cc761a69d715820f01
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-04 14:01:22 +0200

    Create form to save new notes

--- client/views/notes/notes.js
+++ client/views/notes/notes.js
@@ -17,6 +17,22 @@ Template.notes.rendered = function () {
 };
 
 Template.notes.events({
+    'submit form': function (event) {
+        var text = event.target.text.value;
+        event.target.text.value = '';
+
+        var form = event.target.name;
+        // create or search
+        if (form === 'new-note') {
+            Notes.insert({
+                text: text,
+                date: new Date()
+            });
+        } else if (form === 'search-note') {
+            Session.set('noteName', text);
+        }
+        return false;
+    },
     'keyup form[name=search-note]': function (event) {
         var text = event.target.value;
         Session.set('noteName', text);
